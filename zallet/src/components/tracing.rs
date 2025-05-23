10767cc83d0e36c3f5ce53378aa2fb125fac9dde
Jack Grigg <jack@electriccoin.co>
2025-05-23 22:27:35 +0000

    Add `tokio-console` support

--- zallet/src/components/tracing.rs
+++ zallet/src/components/tracing.rs
@@ -1,5 +1,6 @@
+use tracing::level_filters::LevelFilter;
 use tracing_log::LogTracer;
-use tracing_subscriber::FmtSubscriber;
+use tracing_subscriber::{EnvFilter, Layer, layer::SubscriberExt};
 
 use abscissa_core::{Component, FrameworkError, FrameworkErrorKind, terminal::ColorChoice};
 
@@ -10,7 +11,9 @@ pub(crate) struct Tracing {}
 
 impl Tracing {
     pub(crate) fn new(color_choice: ColorChoice) -> Result<Self, FrameworkError> {
-        let filter = std::env::var("RUST_LOG").unwrap_or("info".to_owned());
+        let env_filter = EnvFilter::builder()
+            .with_default_directive(LevelFilter::INFO.into())
+            .from_env_lossy();
 
         // Configure log/tracing interoperability by setting a `LogTracer` as
         // the global logger for the log crate, which converts all log events
@@ -18,15 +21,20 @@ impl Tracing {
         LogTracer::init().map_err(|e| FrameworkErrorKind::ComponentError.context(e))?;
 
         // Construct a tracing subscriber with the supplied filter and enable reloading.
-        let builder = FmtSubscriber::builder()
+        let fmt_layer = tracing_subscriber::fmt::layer()
             .with_ansi(match color_choice {
                 ColorChoice::Always => true,
                 ColorChoice::AlwaysAnsi => true,
                 ColorChoice::Auto => true,
                 ColorChoice::Never => false,
             })
-            .with_env_filter(filter);
-        let subscriber = builder.finish();
+            .with_filter(env_filter);
+
+        let subscriber = tracing_subscriber::registry().with(fmt_layer);
+
+        // Spawn the console server in the background, and apply the console layer.
+        #[cfg(all(feature = "tokio-console", tokio_unstable))]
+        let subscriber = subscriber.with(console_subscriber::spawn());
 
         // Now set it as the global tracing subscriber and save the handle.
         tracing::subscriber::set_global_default(subscriber)
--- zallet/src/task.rs
+++ zallet/src/task.rs
@@ -1,6 +1,7 @@
 /// Spawns a new asynchronous task, returning a `JoinHandle` for it.
 ///
 /// This is a wrapper for [`tokio::task::spawn`] that takes a task name.
+#[cfg(not(tokio_unstable))]
 #[macro_export]
 macro_rules! spawn {
     ( $name:expr, $f:expr ) => {
@@ -8,12 +9,41 @@ macro_rules! spawn {
     };
 }
 
+/// Spawns a new asynchronous task, returning a `JoinHandle` for it.
+///
+/// This is a wrapper for [`tokio::task::spawn`] that takes a task name.
+#[cfg(tokio_unstable)]
+#[macro_export]
+macro_rules! spawn {
+    ( $name:expr, $f:expr ) => {
+        tokio::task::Builder::new()
+            .name($name)
+            .spawn($f)
+            .expect("panic to match tokio::task::spawn")
+    };
+}
+
 /// Runs the provided closure on a thread where blocking is acceptable.
 ///
 /// This is a wrapper for [`tokio::task::spawn_blocking`] that takes a task name.
+#[cfg(not(tokio_unstable))]
 #[macro_export]
 macro_rules! spawn_blocking {
     ( $name:expr, $f:expr ) => {
         tokio::task::spawn_blocking($f)
+    };
+}
+
+/// Runs the provided closure on a thread where blocking is acceptable.
+///
+/// This is a wrapper for [`tokio::task::spawn_blocking`] that takes a task name.
+#[cfg(tokio_unstable)]
+#[macro_export]
+macro_rules! spawn_blocking {
+    ( $name:expr, $f:expr ) => {
+        tokio::task::Builder::new()
+            .name($name)
+            .spawn_blocking($f)
+            .expect("panic to match tokio::task::spawn_blocking")
     };
 }
